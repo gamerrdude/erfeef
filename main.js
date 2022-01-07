d1783fa58fb70cc36e035cc9ec5f56cff735d446
gamerrdude <87709109+gamerrdude@users.noreply.github.com>
2022-01-07 19:44:51 +0530

    Update main.js

--- main.js
+++ main.js
@@ -56,31 +56,31 @@ function modelLoaded() {
 
     if(scoreLeftWrist > 0.2)
     { 
-        circle(rightWristX,rightWristY,20);
+        circle(lefttWristX,leftWristY,20);
 
-        if(rightWristY >0 && rightWristY <= 100)
+        if(leftWristY >0 && leftWristY <= 100)
         {
             document.getElementById("speed").innerHTML = "Speed = 0.5x";  
      
             song.rate(0.1);
         }
-        else if(rightWristY >100 && rightWristY <= 200)
+        else if(lefttWristY >100 && lefttWristY <= 200)
         {
             document.getElementById("speed").innerHTML = "Speed = 1x";      
             song.rate(1);
         }
-        else if(rightWristY >200 && rightWristY <= 300)
+        else if(lefttWristY >200 && leftWristY <= 300)
         {
             document.getElementById("speed").innerHTML = "Speed = 1.5x";        
        
 			song.rate(1.5);
         }
-        else if(rightWristY >300 && rightWristY <= 400)
+        else if(lefttWristY >300 && leftWristY <= 400)
         {
             document.getElementById("speed").innerHTML = "Speed = 2x";      
             song.rate(2);
         }
-        else if(rightWristY >400)
+        else if(leftWristY >400)
         {
             document.getElementById("speed").innerHTML = "Speed = 2.5x";        
             song.rate(2.9);
@@ -89,12 +89,12 @@ function modelLoaded() {
 
     if(scoreRightWrist > 0.2)
     {
-        circle(leftWristX,leftWristY,20);
-        InNumberleftWristY = Number(leftWristY);
-        new_leftWristY = floor(InNumberleftWristY *2);
-        leftWristY_divide_1000 = new_leftWristY/1000;
-        document.getElementById("volume").innerHTML = "Volume = " + leftWristY_divide_1000;     
-        song.setVolume(leftWristY_divide_1000); 
+        circle(rightWristX,rightWristY,20);
+        InNumberrightWristY = Number(righttWristY);
+        new_rightWristY = floor(InNumberrightWristY *2);
+        rightWristY_divide_1000 = new_rightWristY/1000;
+        document.getElementById("volume").innerHTML = "Volume = " + righttWristY_divide_1000;     
+        song.setVolume(righttWristY_divide_1000); 
     }
 
 }
